4b25694f7257353a6bb98055f738033294f4b78d
Kaju-Bubanja <bubanja.kaju@gmail.com>
2015-07-15 00:51:25 +0200

    create DB script

--- createScript.sql
+++ createScript.sql
@@ -1,5 +1,8 @@
 CREATE TABLE laender (Code CHAR(2), Kontinent VARCHAR(12), Land VARCHAR(40), LandFranz VARCHAR(40));
 LOAD DATA LOCAL INFILE 'laender.csv' INTO TABLE laender CHARACTER SET utf8 COLUMNS TERMINATED BY ';';
 
+ALTER TABLE laender ADD latitude FLOAT(10,6);
+ALTER TABLE laender ADD longitude FLOAT(10,6);
+
 CREATE TABLE export (Id INT NOT NULL AUTO_INCREMENT, Code CHAR(2), Art VARCHAR(40), System VARCHAR(10), Kategorie VARCHAR(15), Year Year, Betrag INT, PRIMARY KEY(Id), 
 FOREIGN KEY (Code) REFERENCES laender(Code));
