b75baa422a89a2b46fb135c88986be51f310dade
Kaju-Bubanja <bubanja.kaju@gmail.com>
2015-07-16 18:14:26 +0200

    added swiss kordinates

--- kriegsmaterial/webroot/js/test.js
+++ kriegsmaterial/webroot/js/test.js
@@ -1,4 +1,4 @@
-var x=new google.maps.LatLng(46.94276, 7.44324);
+var schweiz = new google.maps.LatLng(schweizKordinaten[0].Latitude, schweizKordinaten.Longitude);
 var stavanger=new google.maps.LatLng(50.983991,-100.734863);
 var amsterdam=new google.maps.LatLng(52.395715,4.888916);
 var london=new google.maps.LatLng(51.508742,-0.120850);
@@ -10,7 +10,6 @@ var mapProp = {
   zoom:3,
   mapTypeId:google.maps.MapTypeId.HYBRID
   };
-  
 
 var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
@@ -18,8 +17,7 @@ var arrow = {
     path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
   };
 
-
-var myTrip=[london, stavanger];
+var myTrip=[schweiz, london];
 var flightPath=new google.maps.Polyline({
   path:myTrip,
   icons: [{
