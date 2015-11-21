06cf92ea89940c4d187c93b0daa0641a3abf9815
Kaju-Bubanja <bubanja.kaju@gmail.com>
2015-11-22 00:30:19 +0100

    added updated create script

--- createScript.sql
+++ createScript.sql
@@ -33,6 +33,14 @@ CREATE TABLE `art` (
   KEY `Art` (`Art`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created DATETIME,
+    modified DATETIME
+);
+
 INSERT INTO `art` (`Id`, `Art`) VALUES
 (2, 'Besondere Militärische Güter'),
 (3, 'Dual Use Güter'),
